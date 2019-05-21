cdfb68fe63626c926a80e38f4e56ee41a9b6832f
cpcmc <cristiano.clemente.2000@gmail.com>
2019-05-21 09:04:04 +0100

    Create Mood-Cue.ino

---
+++ mood-cue/Mood-Cue.ino
@@ -0,0 +1,34 @@
+//imports the servo library
+#include <Servo.h>
+
+//create instance of servo
+Servo myServo;
+
+int const potPin = A0;
+//input var
+int potVal;
+//output var
+int angle;
+
+void setup() {
+  //servo is attached to pin 9
+  myServo.attach(9);
+  //init serial connection
+  Serial.begin(9600);
+}
+
+void loop() {
+  //read analog input
+  potVal = analogRead(potPin);
+  Serial.print("potVal: ");
+  Serial.print(potVal);
+
+  //0-1023 -> 0-179
+  angle = map(potVal, 0, 1023, 179);
+  Serial.print(", angle: ");
+  Serial.print(angle);
+
+  //move servo
+  myServo.write(angle);
+  delay(15);
+}
